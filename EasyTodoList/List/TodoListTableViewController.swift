3ae6602a630653b6dd12fac676ba64196e7dbe26
saltpaper <saltpaper@126.com>
2016-12-16 15:44:24 +0800

    after deleted need to sort

--- EasyTodoList/List/TodoListTableViewController.swift
+++ EasyTodoList/List/TodoListTableViewController.swift
@@ -186,6 +186,7 @@ final class TodoListTableViewController: UITableViewController {
         }catch {
             print("fetch failed☹️")
         }
+        self.sortBy()
         tableView.reloadData()
         
     }
